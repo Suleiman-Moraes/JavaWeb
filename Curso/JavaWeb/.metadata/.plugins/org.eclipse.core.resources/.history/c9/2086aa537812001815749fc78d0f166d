c305b689bff56775c30a9144196b6c3a9f72c3a9
Suleiman-Moraes <suleimanmoraes@gmail.com>
2018-02-17 12:05:32 -0200

    Persistencia JDBC

---
+++ Curso/JavaWeb/.metadata/.plugins/org.eclipse.core.resources/.history/c9/2086aa537812001815749fc78d0f166d
@@ -0,0 +1,110 @@
+package com.moraes;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class Main2 {
+static Scanner leia = new Scanner(System.in);
+	
+	static long cdCurso = 0;
+	static String nome = ""; 
+	static float valor = 0;
+	static String url = "";
+	
+	static Curso curso;
+	static CursoDAO cursoDAO;
+	
+	public static void main(String[] args) {
+		try {
+			saida(chamaMetodo(combobox()));
+			main(null);
+		} catch (Exception e) {
+			System.out.println("Finalizado!");
+		}
+	}
+	
+	private static void criarCurso() throws Exception {
+		leia.nextLine();
+		
+		System.out.println("Nome: ");
+    	nome = leia.nextLine();
+    	
+    	System.out.println("Email: ");
+    	url = leia.nextLine();
+    	
+    	System.out.println("Código: ");
+    	cdCurso = leia.nextLong();
+    	
+    	System.out.println("Valor: R$");
+    	valor = leia.nextFloat();
+    	
+    	leia.nextLine();
+		
+		curso = new Curso(cdCurso, nome , valor, url);
+    }
+    private static long pegarCodigo() {
+    	System.out.println("Código: ");
+    	return leia.nextLong();
+    }
+    private static int combobox() {
+    	System.out.println("\n\n\nSistema de gerenciamento de Cursos\n"
+				+ "==================================================================================");
+		System.out.println("Digite [1] para Consultar Todos os Cursos");
+		System.out.println("Digite [2] para Consultar um Curso Especifico");
+		System.out.println("Digite [3] para Cadastrar um novo Curso");
+		System.out.println("Digite [4] para Alterar um Curso");
+		System.out.println("Digite [5] para Excluir um Curso");
+		System.out.println("Digite [!(Número)] para Sair\n"
+				+ "==================================================================================");
+		return leia.nextInt();
+    }
+    private static String chamaMetodo(int combobox) throws Exception {
+    	switch (combobox) {
+		case 3:{
+			criarCurso();
+			cursoDAO = new CursoDAO(curso);
+			return cursoDAO.insert();
+		}
+		case 5:{
+			cursoDAO = new CursoDAO();
+			return cursoDAO.delete(pegarCodigo());
+		}
+		case 4:{
+			criarCurso();
+			cursoDAO = new CursoDAO(curso);
+			return cursoDAO.update();
+		}
+		case 2:{
+			cursoDAO = new CursoDAO();
+			return texto(cursoDAO.getSingle(pegarCodigo()));
+		}
+		case 1:{
+			cursoDAO = new CursoDAO();
+			return texto(cursoDAO.getAll());
+		}
+		default : {
+			throw new Exception("Fim");
+		}
+    	}
+    	
+    }
+    private static void saida(String texto) {
+    	System.out.println(texto);
+    }
+    private static String texto(List<Curso> list) {
+    	String tudo = "";
+    	for(Curso x : list) {
+    		tudo += x.toString();
+    		tudo += ("\n=================================================================\n\n");
+     	}
+    	return tudo;
+    }
+    private static String texto(Curso ...curso) {
+    	String tudo = "";
+    	for(Curso x : curso) {
+    		tudo += x.toString();
+    		tudo += ("\n=================================================================\n\n");
+     	}
+    	return tudo;
+    }
+}
---
+++ Curso/JavaWeb/3Disciplina/src/com/moraes/MainPagamento.java
@@ -0,0 +1,118 @@
+package com.moraes;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Scanner;
+
+public class MainPagamento {
+static Scanner leia = new Scanner(System.in);
+	
+	static long cpf = 0;
+	static long cdCurso = 0; 
+	static Date data;
+	
+	static Pagamento pagamento;
+	static PagamentoDAO pagamentoDAO;
+	
+	public static void main(String[] args) {
+		try {
+			saida(chamaMetodo(combobox()));
+			main(null);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Finalizado!");
+		}
+	}
+	
+	private static void criarPagamento() throws Exception {
+		try {
+			leia.nextLine();
+			
+			System.out.println("Codigo do Curso: ");
+	    	cdCurso = leia.nextLong();
+	    	
+	    	leia.nextLine();
+	    	
+	    	System.out.println("Data: ");
+	    	data = Date.valueOf(leia.nextLine());
+	    	
+	    	System.out.println("CPF: ");
+	    	cpf = leia.nextLong();
+	    	
+	    	leia.nextLine();
+			
+			pagamento = new Pagamento(cpf, cdCurso , data);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    }
+    private static long pegarCpf() {
+    	System.out.println("CPF: ");
+    	return leia.nextLong();
+    }
+    private static long pegarCdCurso() {
+    	System.out.println("Código do Curso: ");
+    	return leia.nextLong();
+    }
+    private static int combobox() {
+    	System.out.println("\n\n\nSistema de gerenciamento de Pagamentos\n"
+				+ "==================================================================================");
+		System.out.println("Digite [1] para Consultar Todos os Pagamentos");
+		System.out.println("Digite [2] para Consultar um Pagamento Especifico");
+		System.out.println("Digite [3] para Cadastrar um novo Pagamento");
+		System.out.println("Digite [4] para Alterar um Pagamento");
+		System.out.println("Digite [5] para Excluir um Pagamento");
+		System.out.println("Digite [!(Número)] para Sair\n"
+				+ "==================================================================================");
+		return leia.nextInt();
+    }
+    private static String chamaMetodo(int combobox) throws Exception {
+    	switch (combobox) {
+		case 3:{
+			criarPagamento();
+			pagamentoDAO = new PagamentoDAO(pagamento);
+			return pagamentoDAO.insert();
+		}
+		case 5:{
+			pagamentoDAO = new PagamentoDAO();
+			return pagamentoDAO.delete(pegarCdCurso(), pegarCpf());
+		}
+		case 4:{
+			criarPagamento();
+			pagamentoDAO = new PagamentoDAO(pagamento);
+			return pagamentoDAO.update();
+		}
+		case 2:{
+			pagamentoDAO = new PagamentoDAO();
+			return texto(pagamentoDAO.getSingle(pegarCdCurso(), pegarCpf()));
+		}
+		case 1:{
+			pagamentoDAO = new PagamentoDAO();
+			return texto(pagamentoDAO.getAll());
+		}
+		default : {
+			throw new Exception("Fim");
+		}
+    	}
+    	
+    }
+    private static void saida(String texto) {
+    	System.out.println(texto);
+    }
+    private static String texto(List<Pagamento> list) {
+    	String tudo = "";
+    	for(Pagamento x : list) {
+    		tudo += x.toString();
+    		tudo += ("\n=================================================================\n\n");
+     	}
+    	return tudo;
+    }
+    private static String texto(Pagamento ...paga) {
+    	String tudo = "";
+    	for(Pagamento x : paga) {
+    		tudo += x.toString();
+    		tudo += ("\n=================================================================\n\n");
+     	}
+    	return tudo;
+    }
+}
